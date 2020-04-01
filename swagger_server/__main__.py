3e8995a3589f0014403ee1eb0538417e0775dece
nucklehead <pierevans@gmail.com>
2020-04-01 03:01:39 -0400

    Docker fix and config load

--- swagger_server/__main__.py
+++ swagger_server/__main__.py
@@ -1,13 +1,15 @@
 #!/usr/bin/env python3
 
 import connexion
+import os
 
 from swagger_server import encoder
-from swagger_server.database.config import configure_database
+from swagger_server.database.config import configure_database, update_yaml_config_with_env
 
 
 def main():
-    app = connexion.App('resource_access_service', specification_dir='./swagger/')
+    update_yaml_config_with_env()
+    app = connexion.App('resource_access_service', specification_dir='./swagger_server/swagger/')
     configure_database(app.app)
     app.app.json_encoder = encoder.JSONEncoder
     app.add_api('swagger.yaml', arguments={'title': 'resource-access-service'}, pythonic_params=True)
--- swagger_server/database/config.py
+++ swagger_server/database/config.py
@@ -1,12 +1,30 @@
 import os
+import yaml
 
 from connexion import FlaskApp
 from flask_restplus_data import FlaskData
+from jsonpath_ng import jsonpath, parse
 
 from swagger_server import resources
 from swagger_server.resources import migrations
 
 flask_data = FlaskData(os.path.dirname(resources.__file__))
+
+
+def override_with_env(config):
+    for key, val in os.environ.items():
+        path = f'$.{key.lower().replace("_", ".")}'
+        jsonpath_expr = parse(path)
+        jsonpath_expr.find(config)
+        jsonpath_expr.update(config, val)
+
+
+def update_yaml_config_with_env():
+    with open(os.path.join(os.path.dirname(resources.__file__), 'config.yaml'), 'r+') as config_file:
+        config = yaml.load(config_file)
+        override_with_env(config)
+        config_file.seek(0)
+        yaml.dump(config, config_file)
 
 
 def configure_database(app: FlaskApp):
